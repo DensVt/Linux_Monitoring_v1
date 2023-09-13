c2e4b8c0c0a247eb699494cc06672718425f979d
DensVt <rosbykar@student.21-school.ru>
2023-09-13 11:26:45 +0300

    01 - 03 update

---
+++ src/03/stats.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Определение функции get_stats, которая собирает статистику о системе
+get_stats() {
+    # Получение имени хоста
+    HOSTNAME=$(hostname)
+    # Получение часового пояса и UTC смещения
+    TIMEZONE=$(printf "%s UTC %s" "$(cat /etc/timezone)" "$(date +"%-:::z")")
+    # Получение имени текущего пользователя
+    USER=$(whoami)
+    # Получение названия и версии операционной системы
+    OS_NAME=$(grep "NAME=" /etc/os-release | head -1 | cut -d'=' -f2 | tr -d '"')
+    OS_VERSION=$(grep "VERSION=" /etc/os-release | head -1 | cut -d'=' -f2 | tr -d '"' | tr -d '(' | tr -d ')')
+    OS="$OS_NAME $OS_VERSION"
+    # Получение текущей даты и времени
+    DATE=$(date +"%d %B %Y %H:%M:%S")
+    # Получение времени работы системы
+    UPTIME=$(uptime -p | sed 's/up //')
+    # Получение времени работы системы в секундах
+    UPTIME_SEC=$(cat /proc/uptime | awk '{print $1}' | cut -d'.' -f1)
+    # Получение IP-адреса
+    IP=$(hostname -I | cut -d' ' -f1)
+    # Получение маски подсети
+    MASK=$(ip a | ipcalc "$IP" | grep "^Netmask" | awk '{print $2}')
+    # Получение адреса шлюза
+    GATEWAY=$(ip route | awk '/default/ {print $3}')
+    
+    # Вывод собранных данных
+    echo "$HOSTNAME"
+    echo "$TIMEZONE"
+    echo "$USER"
+    echo "$OS"
+    echo "$DATE"
+    echo "$UPTIME"
+    echo "$UPTIME_SEC"
+    echo "$IP"
+    echo "$MASK"
+    echo "$GATEWAY"
+
+    # Сбор и вывод данных о памяти
+    Mem=($(free -b | grep Mem))
+    echo -n $(echo "scale=3;x=${Mem[1]}/1073741824; if(x<1) print 0; x" | bc); echo " GB"
+    echo -n $(echo "scale=3;x=${Mem[2]}/1073741824; if(x<1) print 0; x" | bc); echo " GB"
+    echo -n $(echo "scale=3;x=${Mem[3]}/1073741824; if(x<1) print 0; x" | bc); echo " GB"
+    
+    # Сбор и вывод данных о дисковом пространстве
+    Space=($(df -B1 / | grep /))
+    echo -n  $(echo "scale=2;x=${Space[1]}/1048576; if(x<1) print 0; x" | bc); echo " MB"
+    echo -n  $(echo "scale=2;x=${Space[2]}/1048576; if(x<1) print 0; x" | bc); echo " MB"
+    echo -n  $(echo "scale=2;x=${Space[3]}/1048576; if(x<1) print 0; x" | bc); echo " MB"
+}
